ef576765164a3a465b44ef34c6759268a72e7437
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2019-02-14 00:32:16 -0500

    Merge flash messages into cookie

--- frontend/utils/flash.ts
+++ frontend/utils/flash.ts
@@ -1,9 +1,17 @@
-import cookie from 'js-cookie';
 import { IContext, flashType } from '../@types';
 import { getCookie, removeCookie, setCookie } from './session';
 
 export const set = (value: flashType, ctx?: IContext, options?: any) => {
-	setCookie('flash', value, ctx, options);
+	const val = getCookie('flash', ctx) || {};
+	setCookie(
+		'flash',
+		{
+			...val,
+			...value
+		},
+		ctx,
+		options
+	);
 };
 
 export const get = (ctx?: IContext): flashType => {
