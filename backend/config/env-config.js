312e6877dc73e9283904f0f7482c38790b168af4
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2019-03-08 22:55:40 -0800

    Updated docker mongo url

--- backend/config/env-config.js
+++ backend/config/env-config.js
@@ -8,7 +8,7 @@ const sharedConfig = {
 };
 
 const serverRuntimeConfig = {
-	DB: env.DB || 'mongodb://localhost:27017/HelloWorld',
+	DB: env.DB || 'mongodb://0.0.0.0:27017/HelloWorld',
 	EMAIL: env.EMAIL || 'my@email.com',
 	EXPIRES_IN: env.EXPIRES_IN || '7 days',
 	GC_BUCKET: env.GC_BUCKET || 'mybucket',
@@ -17,7 +17,7 @@ const serverRuntimeConfig = {
 	GC_PROJECT_ID: env.GC_PROJECT_ID || 'myprojectid',
 	NODE_ENV: env.NODE_ENV || 'development',
 	ORG_NAME: env.ORG_NAME || 'Purdue Hackers',
-	REDIS_URL: env.REDIS_URL || 'redis://localhost:6379',
+	REDIS_URL: env.REDIS_URL || 'redis://0.0.0.0:6379',
 	SECRET: env.SECRET || 'my-secret',
 	SENDGRID_KEY: env.SENDGRID_KEY || 'mysendgridkey',
 	...sharedConfig
