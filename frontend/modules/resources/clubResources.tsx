eb25a88c0b6f7d4284edd0827f857522a6e15d2f
aakarshitpandey <aakarshit.pandey7@gmail.com>
2019-05-13 15:08:32 +0530

    Changed the data structure to directly sort and display the workshops. closes #48, closes #96

--- frontend/modules/resources/clubResources.tsx
+++ frontend/modules/resources/clubResources.tsx
@@ -6,17 +6,32 @@ export interface IResourcePage {
   image: any;
 }
 
-const ClubResources = ({ resources }: { resources: IResourcePage[] }) => {
+export interface IWorkshop {
+  title: string,
+  link: string,
+  technology: string,
+}
+
+export const technology = [{ name: "WebDev(Backend)", img: 'static/images/nodejslogo.png' },
+{ name: "Android App Dev", img: 'static/images/android.png', },
+{ name: "WebDev(Frontend)", img: 'static/images/logo.svg', },
+{ name: "iOS App Dev", img: 'static/images/iOS.png', },
+{ name: "Sample Projects", img: 'static/images/express-node-mongo.png', },]
+
+const ClubResources = ({ resources }: { resources: IWorkshop[] }) => {
   return (
     <div>
       <br />
       <ul id="alerts" className="res-array">
-        {resources.map(resource => {
-          const workshops = resource.workshop.map(workshop => {
+        {technology.map(workshop => {
+          const relevantWorkshops = resources.filter(res => {
+            return res.technology.localeCompare(workshop.name) === 0;
+          })
+          const workshops = relevantWorkshops.map(resource => {
             return (
               <li className="feild-name">
-                <a href={workshop.link} target="_blank">
-                  {workshop.title}
+                <a href={resource.link} target="_blank">
+                  {resource.title}
                 </a>
               </li>
             );
@@ -26,11 +41,11 @@ const ClubResources = ({ resources }: { resources: IResourcePage[] }) => {
             <li className="pallete">
               <ul>
                 <li className="feild">
-                  <div className="title-text">{resource.name}</div>
+                  <div className="title-text">{workshop.name}</div>
                 </li>
                 <li className="feild">
                   <br />
-                  <img src={resource.image} alt="image here" />
+                  <img src={workshop.img} alt="image here" />
                   <br />
                 </li>
                 {workshops}
--- frontend/modules/resources/resources.tsx
+++ frontend/modules/resources/resources.tsx
@@ -1,121 +1,51 @@
 import * as React from "react";
 import "./index.css";
-import ClubResources, { IResourcePage } from "./clubResources";
+import ClubResources, { IResourcePage, IWorkshop, technology } from "./clubResources";
 
-const res: IResourcePage[] = [
+const workshops: IWorkshop[] = [
   {
-    name: "Purdue Hackers",
-    workshop: [
-      {
-        title: "Nodejs Workshop",
-        link:
-          "https://purduehackers.gitbook.io/tutorials/workshops/nodejs-workshop"
-      },
-    ],
-    image: "static/images/nodejslogo.png",
+    title: "Nodejs Workshop",
+    link: "https://purduehackers.gitbook.io/tutorials/workshops/nodejs-workshop",
+    technology: "WebDev(Backend)",
   },
   {
-    name: "SIG Apps",
-    workshop: [
-      {
-        title: "Android for Beginners",
-        link: "https://vidia.gitbooks.io/hello-android/content/building/adding_text_input.html"
-      }
-    ],
-    image: "static/images/android.png"
+    title: "Android for Beginners",
+    link: "https://vidia.gitbooks.io/hello-android/content/building/adding_text_input.html",
+    technology: "Android App Dev",
   },
   {
-    name: "Backend / Database",
-    workshop: [
-      {
-        title: "Making a Server",
-        link: "https://nodejs.org/en/"
-      }
-    ],
-    image: "static/images/express-node-mongo.png"
+    title: "HTML, CSS & JS",
+    link: "https://maneesht.gitbooks.io/web-development-basics/content/",
+    technology: "WebDev(Frontend)",
   },
   {
-    name: "Purdue WebDev Club",
-    workshop: [
-      {
-        title: "WebDev Basics Workshop",
-        link: "https://maneesht.gitbooks.io/web-development-basics/content/"
-      },
-      {
-        title: "Full Stack Development",
-        link: "https://maneesht.gitbooks.io/hello-spotify/content/"
-      }
-    ],
-    image: "static/images/webDevClub.png"
+    title: "React Workshop",
+    link: "https://purduehackers.gitbook.io/tutorials/workshops/react-workshop",
+    technology: "WebDev(Frontend)",
   },
   {
-    name: "Purdue Hackers",
-    workshop: [
-      {
-        title: "React Workshop",
-        link: "https://purduehackers.gitbook.io/tutorials/workshops/react-workshop"
-      }
-    ],
-    image: "static/images/logo.svg"
+    title: "Swift and Objective-C",
+    link: "https://nodejs.org/en/",
+    technology: "iOS App Dev",
   },
   {
-    name: "iOS Dev Club",
-    workshop: [
-      {
-        title: "Node.js Workshop",
-        link: "https://nodejs.org/en/"
-      }
-    ],
-    image: "static/images/iOS.jpg"
+    title: "Making a Server",
+    link: "https://nodejs.org/en/",
+    technology: "Sample Projects"
   },
-  // {
-  //   name: "res4",
-  //   workshop: [
-  //     {
-  //       title: "Node.js Workshop",
-  //       link: "https://nodejs.org/en/"
-  //     }
-  //   ],
-  //   image: "#"
-  // },
-  // {
-  //   name: "res4",
-  //   workshop: [
-  //     {
-  //       title: "Node.js Workshop",
-  //       link: "https://nodejs.org/en/"
-  //     }
-  //   ],
-  //   image: "#"
-  // },
-  // {
-  //   name: "res4",
-  //   workshop: [
-  //     {
-  //       title: "Node.js Workshop",
-  //       link: "https://nodejs.org/en/"
-  //     }
-  //   ],
-  //   image: "#"
-  // },
-  // {
-  //   name: "res4",
-  //   workshop: [
-  //     {
-  //       title: "Node.js Workshop",
-  //       link: "https://nodejs.org/en/"
-  //     }
-  //   ],
-  //   image: "#"
-  // }
-];
+  {
+    title: "Hello-Spotify",
+    link: "https://maneesht.gitbooks.io/hello-spotify/content/",
+    technology: "Sample Projects"
+  }
+]
 
 export default class Resources extends React.Component<any, any> {
   public render() {
     return (
       <div>
         <br />
-        <ClubResources resources={res} />
+        <ClubResources resources={workshops} />
       </div>
     );
   }
