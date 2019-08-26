0d9d8e4df601510e10c1f5b4581f657cf348f5ed
aakarshitpandey <aakarshit.pandey7@gmail.com>
2019-08-25 23:43:47 -0400

    Fixed the styling background of login-page

--- frontend/modules/login/LoginPage.tsx
+++ frontend/modules/login/LoginPage.tsx
@@ -46,7 +46,7 @@ const Login = ({ signin, flashError, flashSuccess, clear }: Props) => {
 	return (
 		<div
 			className="yo uk-section uk-section-primary uk-flex uk-flex-center bg-purple-gradient"
-			style={{}}
+			style={{ minHeight: '100vh' }}
 		>
 			<div className="uk-container-small fullwidth uk-margin-large-bottom">
 				<h2 className="h1-light text-yellow">Login Page</h2>
