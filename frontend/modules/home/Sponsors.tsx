f2a0e144c6959807978f65cde2f49f8be2518dff
Ashwin Gokhale <gokhale0@purdue.edu>
2019-08-26 03:08:15 -0400

    Updated sponsorship email

--- frontend/modules/home/Sponsors.tsx
+++ frontend/modules/home/Sponsors.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export const Sponsors = () => (
-	<div className="uk-section uk-flex uk-flex-around uk-flex-wrap bg-purple-gradient" id="sponsors">
+	<div
+		className="uk-section uk-flex uk-flex-around uk-flex-wrap bg-purple-gradient"
+		id="sponsors"
+	>
 		<div className="uk-container-small fullwidth">
 			<div className="uk-container-small fullwidth">
 				<h2 className="h2-light text-align-center text-yellow">Sponsors</h2>
@@ -9,7 +12,13 @@ export const Sponsors = () => (
 			<div className="uk-container-small fullwidth">
 				<p className="text-yellow text-align-center">
 					If you are interested in sponsoring Hello World 2018, email us at{' '}
-					<a className="uk-link-text" href="mailto:helloworld@purduehackers.com" style={{ color: 'white' }}>helloworld@purduehackers.com</a>{' '}
+					<a
+						className="uk-link-text"
+						href="mailto:helloworldpurdue@gmail.com"
+						style={{ color: 'white' }}
+					>
+						helloworldpurdue@gmail.com
+					</a>{' '}
 					for more details!
 				</p>
 			</div>
