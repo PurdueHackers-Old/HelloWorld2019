5039becbf10e0c86dfd85603eea7c2442bc1c53b
Ashwin Gokhale <gokhale0@purdue.edu>
2019-08-26 15:19:32 -0400

    Updated apply page

--- frontend/modules/apply/ApplyPage.tsx
+++ frontend/modules/apply/ApplyPage.tsx
@@ -94,7 +94,7 @@ const Apply = ({ user, flashError, flashSuccess, clear }: Props) => {
 				id="apply-heading"
 				className="text-yellow uk-heading-small uk-margin-large-top uk-margin-large-left uk-padding-large-top"
 			>
-				Apply Page
+				Apply
 			</h1>
 			<div
 				className="foreground-announcement"
@@ -116,7 +116,7 @@ const Apply = ({ user, flashError, flashSuccess, clear }: Props) => {
 					</div>
 				)}
 				{state.statusPublic && (
-					<div className="uk-margin-large-top uk-margin-large-left">
+					<div className="uk-margin-large-left">
 						<div className="text-yellow">
 							Status:
 							<br />
