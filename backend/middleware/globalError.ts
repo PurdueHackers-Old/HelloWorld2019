11d25593da47c0cd9fb10dcb2a0bf798d481c5a7
Ashwin Gokhale <gokhale0@purdue.edu>
2019-08-26 23:55:27 -0400

    Catch multer errors

--- backend/middleware/globalError.ts
+++ backend/middleware/globalError.ts
@@ -1,5 +1,7 @@
 import { getFromContainer } from 'routing-controllers';
 import { AuthorizationRequiredError } from 'routing-controllers/error/AuthorizationRequiredError';
+import { Request, Response, NextFunction } from 'express';
+import { MulterError } from 'multer';
 import { errorRes } from '../utils';
 import { createLogger } from '../utils/logger';
 import { EmailService } from '../services/email.service';
@@ -8,12 +10,13 @@ import CONFIG from '../config';
 const logger = createLogger('GlobalError');
 const emailService = getFromContainer(EmailService);
 
-export const globalError = (err, req, res, next) => {
+export const globalError = (err, req: Request, res: Response, next: NextFunction) => {
 	let { message, httpCode } = err;
 	message = message || 'Whoops! Something went wrong!';
 	httpCode = httpCode || 500;
+
 	// Send an email if error is from server
-	if (httpCode === 500) {
+	if (!(err instanceof MulterError) && httpCode === 500) {
 		logger.fatal('Unhandled exception:', err);
 		if (CONFIG.NODE_ENV === 'production')
 			emailService
@@ -25,7 +28,5 @@ export const globalError = (err, req, res, next) => {
 		if (err instanceof AuthorizationRequiredError) message = 'You must be logged in!';
 		logger.error('Caught error:', message);
 		errorRes(res, httpCode, message);
-
-		// next(err);
 	}
 };
