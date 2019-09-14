28a1a3d86459d8d8cf16a95cb930d9fd721821aa
Aakarshit Pandey <42846141+aakarshitpandey@users.noreply.github.com>
2019-09-14 13:11:24 -0400

    fixed time stamp (#213)
    * Changed the data structure to directly sort and display the workshops. closes #48, closes #96
    
    * Changed DB to MONGODB_URI (#152)
    
    * Merged
    
    * Landing (#153)
    
    * Changed DB to MONGODB_URI
    
    * Switched to scss
    
    * Removed local fonts
    
    * Working on uikit landing page
    
    * Fixed broken tests
    
    * Landing (#154)
    
    * Changed DB to MONGODB_URI
    
    * Switched to scss
    
    * Removed local fonts
    
    * Working on uikit landing page
    
    * Fixed broken tests
    
    * Updated navbar
    
    * Changed font loading to link
    
    * Fixed broken tests
    
    * updated the fork
    
    * Landign page completed
    
    * styled the nav bar
    
    * landing page
    
    * Announcement page styling
    
    * Styled the announcements page
    
    * Fixed Navbar
    
    * fixed the syntax
    
    * Fixed broken tests
    
    * landing page (#155)
    
    * Changed the data structure to directly sort and display the workshops. closes #48, closes #96
    
    * Merged
    
    * updated the fork
    
    * Landign page completed
    
    * styled the nav bar
    
    * landing page
    
    * Announcement page styling
    
    * Styled the announcements page
    
    * Fixed Navbar
    
    * fixed the syntax
    
    * Fixed broken tests
    
    * Patches (#156)
    
    * Fixed typo
    
    * Closes #142
    
    * Closes #133
    
    * Changed default tracking id
    
    * Sorted imports
    
    * Fixed tests
    
    * Make tests run faster
    
    * Added compression
    
    * Removed unused fonts
    
    * fixed contrasts
    
    * Added styling for signup, checkin, forgotpw, login, edit profile and profile page (#157)
    
    * Added styling for apply page
    
    * Revert "Merge branch 'staging' into master"
    
    This reverts commit 53841ec9427e01573e4c74b10584b67c1232c133, reversing
    changes made to b78ee7694411c311f1bde9b11345c8914ea48d3e.
    
    * Revert "Revert "Merge branch 'staging' into master""
    
    This reverts commit 71e6a330e7f9963f85c5efa6b552fd4821687b6c.
    
    * Fixed the styling background of login-page
    
    * Patches (#159)
    
    * Fixed typo
    
    * Closes #142
    
    * Closes #133
    
    * Changed default tracking id
    
    * Sorted imports
    
    * Fixed tests
    
    * Make tests run faster
    
    * Added compression
    
    * Removed unused fonts
    
    * Updated anchors in nav to nextjs links
    
    * Added uikit js script
    
    * Updated global styles
    
    * Updated public pages
    
    * Fixed broken tests
    
    * Added design to announcements and dashboard
    
    * Styled the qr page
    
    * Styled the ancmt manage page
    
    * Fixed merge conflicts
    
    * Removed application tests
    
    * Fixed the announcements timestamp
    
    * Added the announcements tab
    
    * fixed ancmnt responsiveness
    
    * fixed timestamp

--- frontend/modules/announcements/Announcement.tsx
+++ frontend/modules/announcements/Announcement.tsx
@@ -37,8 +37,14 @@ export default (props: Props) => {
 	const { _id, title, body, labels, createdAt, released, admin,} = props;
 	let date = new Date(createdAt)
 	let hours = date.getHours()
-	if (hours >= 12) {
+	if (hours > 12) {
 		hours = hours - 12
+	} else if (hours === 0) {
+		hours = 12
+	}
+
+	const formatTime = (date: Date) => {
+		return date.toLocaleString('en-US', {hour12: true, hour: '2-digit', minute: '2-digit',})
 	}
 
 	return (
@@ -50,9 +56,7 @@ export default (props: Props) => {
 			<div className="uk-flex">
 				<div className={`text-white label label-${labels}`}>{labels}</div>
 				<div className="uk-align-right timestamp text-white label">
-					{`${hours < 10 ? "0" + hours: hours}:
-					 	${date.getMinutes() < 10 ? "0" + date.getMinutes(): date.getMinutes()}
-						${date.getHours() < 12 ? "AM": "PM"}`}
+					{formatTime(date)}
 				</div>
 			</div>
 			<h3 className="uk-card-title text-yellow margin-small">{title}</h3>
